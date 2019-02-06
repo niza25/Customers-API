595645ceb1925e724b619598798b52fdc9c4d9eb
Izabela <izabela.nowosada@gmail.com>
2019-02-06 13:03:25 +0100

    Fix first auth

--- auth/routes.js
+++ auth/routes.js
@@ -1,17 +1,16 @@
 const { Router } = require('express')
 const router = new Router()
 const { toJWT, toData } = require('./jwt')
-const jwt = require('jsonwebtoken')
 
-router.post('/logins', (req, res, next) => {
-  const email = req.body.email;
-  const psw = req.body.password;
-  if(!email && !psw){
-    return res.status(400).send({
+router.post('/logins', (req, res) => {
+  const email = req.body.email
+  const psw = req.body.password
+  if(!email || !psw){
+    res.status(400).send({
       message: 'Please supply a valid email and password'
     })
   }
-  return res.send({
+  res.send({
     jwt: toJWT({ userId: 1 })
   })
 })
--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const customersRouter = require('./customers/routes')
 const companiesRouter = require('./companies/routes')
-const loginRouter = require('./auth/routes')
+const authRouter = require('./auth/routes')
 
 const app = express()
 const port = process.env.PORT || 4000
 
 app
   .use(bodyParser.json())
-  .use(loginRouter)
+  .use(authRouter)
   .use(customersRouter)
   .use(companiesRouter)
   .listen(port, () => console.log(`Listening on port ${port}`))
