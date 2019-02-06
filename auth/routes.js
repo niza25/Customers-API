c743abb5d9cc2cca9ee03f82e2f83d4972a869fb
Izabela <izabela.nowosada@gmail.com>
2019-02-06 14:16:08 +0100

    Add signup endpoint

--- auth/routes.js
+++ auth/routes.js
@@ -15,4 +15,28 @@ router.post('/logins', (req, res) => {
   })
 })
 
+// secret-endpoint
+router.get('/secret-endpoint', (req, res) => {
+  const auth = req.headers.authorization && req.headers.authorization.split(' ')
+  if (auth && auth[0] === 'Bearer' && auth[1]) {
+    try {
+      const data = toData(auth[1])
+      res.send({
+        message: 'Thanks for visiting the secret endpoint.',
+        data
+      })
+    }
+    catch(error) {
+      res.status(400).send({
+        message: `Error ${error.name}: ${error.message}`,
+      })
+    }
+  }
+  else {
+    res.status(401).send({
+      message: 'Please supply some valid credentials'
+    })
+  }
+})
+
 module.exports = router
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser')
 const customersRouter = require('./customers/routes')
 const companiesRouter = require('./companies/routes')
 const authRouter = require('./auth/routes')
+const usersRouter = require('./users/routes')
 
 const app = express()
 const port = process.env.PORT || 4000
@@ -10,6 +11,7 @@ const port = process.env.PORT || 4000
 app
   .use(bodyParser.json())
   .use(authRouter)
+  .use(usersRouter)
   .use(customersRouter)
   .use(companiesRouter)
   .listen(port, () => console.log(`Listening on port ${port}`))
