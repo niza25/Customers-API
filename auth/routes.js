3e5292245e07499078fec4637df1638cf8f6a2d2
Izabela <izabela.nowosada@gmail.com>
2019-02-06 14:42:27 +0100

    Finalize login

--- auth/routes.js
+++ auth/routes.js
@@ -1,18 +1,53 @@
 const { Router } = require('express')
 const router = new Router()
 const { toJWT, toData } = require('./jwt')
+const bcrypt = require('bcrypt');
+const User = require ('../users/model')
 
 router.post('/logins', (req, res) => {
   const email = req.body.email
   const psw = req.body.password
-  if(!email || !psw){
+  if (!email || !psw) {
     res.status(400).send({
       message: 'Please supply a valid email and password'
     })
+  } else {
+    // 1. find user based on email address
+    User
+      .findOne({
+        where: {
+          email: req.body.email
+        }
+      })
+      .then(entity => {
+        if (!entity) {
+          res.status(400).send({
+            message: 'User with that email does not exist'
+          })
+        }
+        // 2. use bcrypt.compareSync to check the password against the stored hash
+        if (bcrypt.compareSync(req.body.password, entity.password)) {
+          // 3. if the password is correct, return a JWT with the userId of the user (user.id)
+          res.send({
+            jwt: toJWT({ userId: entity.id })
+          })
+        }
+        else {
+          res.status(400).send({
+            message: 'Password was incorrect'
+          })
+        }
+      })
+      .catch(err => {
+        console.error(err)
+        res.status(500).send({
+          message: 'Something went wrong'
+        })
+      })
+    /* res.send({
+      jwt: toJWT({ userId: 1 })
+    }) */
   }
-  res.send({
-    jwt: toJWT({ userId: 1 })
-  })
 })
 
 // secret-endpoint
@@ -26,7 +61,7 @@ router.get('/secret-endpoint', (req, res) => {
         data
       })
     }
-    catch(error) {
+    catch (error) {
       res.status(400).send({
         message: `Error ${error.name}: ${error.message}`,
       })
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser')
 const customersRouter = require('./customers/routes')
 const companiesRouter = require('./companies/routes')
 const authRouter = require('./auth/routes')
-const usersRouter = require('./Users/routes')
+const usersRouter = require('./users/routes')
 
 const app = express()
 const port = process.env.PORT || 4000
