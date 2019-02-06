08d9ef1fecb660c6527555f7a752779fed09f06c
Izabela <izabela.nowosada@gmail.com>
2019-02-06 10:41:52 +0100

    Add companies model and connect

---
+++ companies/model.js
@@ -0,0 +1,26 @@
+const Sequelize = require('sequelize')
+const sequelize = require('../db')
+
+const Company = sequelize.define('companies', {
+  name: {
+    type: Sequelize.STRING,
+    field: 'name',
+    allowNull: false
+  },
+  foundingYear: {
+    type: Sequelize.INTEGER,
+    field: 'founding_year',
+    allowNull: false
+  },
+  description: {
+    type: Sequelize.STRING,
+    field: 'description',
+    allowNull: true
+  },
+},
+{
+  timestamps: false,
+  tableName: 'companies'
+})
+
+module.exports = Company
